08a1492e623eb54dbd95a3faeabdc0ceb03cc5fb
Felipe Cipolato <cipolato.palestart@outlook.com>
2019-12-29 22:25:20 -0300

    Add Axios and Dynamic Page/Routing

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,29 @@
+import axios from 'axios';
 import Layout from '../components/MyLayout';
+import Link from 'next/link';
 
-const Index = () => (
+const Index = ({ shows }) => (
   <Layout>
-    <p>Hello Next</p>
+    <h1>Batman TV Shows</h1>
+    <ul>
+      {shows.map(show => (
+        <li key={show.id}>
+          <Link href='/p/[id]' as={`/p/${show.id}`}>
+            <a>{show.name}</a>
+          </Link>
+        </li>
+      ))}
+    </ul>
   </Layout>
 );
+
+Index.getInitialProps = async () => {
+  const res = await axios.get('https://api.tvmaze.com/search/shows?q=batman');
+  const data = res.data;
+
+  return {
+    shows: data.map(el => el.show),
+  }
+}
 
 export default Index;
---
+++ pages/p/[id].js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+import Layout from '../../components/MyLayout';
+
+const Post = ({ show }) => (
+  <Layout>
+    <h1>{show.name}</h1>
+    <p>{show.summary.replace(/<[/]?[pb]>/g, '')}</p>
+    <img src={show.image.medium} />
+  </Layout>
+);
+
+Post.getInitialProps = async (ctx) => {
+  const { id } = ctx.query
+  const res = await axios.get(`https://api.tvmaze.com/shows/${id}`);
+  const show = res.data;
+
+  return {
+    show,
+  }
+};
+
+export default Post;
